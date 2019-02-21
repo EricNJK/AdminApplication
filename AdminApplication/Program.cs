369e8f59365ce215db4259ad2fef5c1d65fc6329
Eric Njoroge <kamau.eric2016@jkuatuni.onmicrosoft.com>
2019-02-21 12:56:21 +0300

    Stage 4

--- AdminApplication/Program.cs
+++ AdminApplication/Program.cs
@@ -15,7 +15,7 @@ namespace AdminApplication
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmAdmin());
         }
     }
 }
